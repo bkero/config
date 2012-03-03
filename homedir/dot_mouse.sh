33eee999d2ee31ea9addc9b797efd17b505c1301
bkero <bkero@Archimedes.(none)>
2012-03-02 16:33:27 -0800

    ultrabook preferences

---
+++ homedir/dot_mouse.sh
@@ -0,0 +1,30 @@
+# higher sensitivity
+synclient FingerLow=9 FingerHigh=12
+
+# faster movements
+synclient MinSpeed=0.5 MaxSpeed=3.5 AccelFactor=0.1
+
+# acceleration factor
+synclient AccelFactor=5.0
+
+# intertia factors
+#synclient HorizHysteresis=0.5 VertHysteresis=0.5
+
+# 2 fingers scroll
+synclient VertTwoFingerScroll=1 HorizTwoFingerScroll=1
+
+# faster coasting
+synclient CoastingSpeed=10 CoastingFriction=25
+
+# enable tap to click (2 fingers for middle click, 3 fingers for right click)
+#synclient TapButton1=1 TapButton2=3 TapButton3=2
+synclient TapButton1=0 TapButton2=0 TapButton3=0
+
+# fuck yea click taps
+synclient ClickFinger2=3 ClickFinger3=2
+
+# faster tap and double tap
+synclient FastTaps=1 MaxDoubleTapTime=100
+
+# set coasting speed
+synclient CoastingSpeed=20 CoastingFriction=50
